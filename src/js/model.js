4753ed64080c8e756d4b3638edef7592fff5ae2d
adnan2212 <73057698+adnan2212@users.noreply.github.com>
2021-12-18 10:22:03 +0530

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -45,34 +45,6 @@ export const loadRecipe = async function (id) {
   }
 };
 
-/*
-export const loadRecipe = async function (id) {
-  // 1) Loading recipe
-  try {
-    const data = await getJSON(`${API_URL}${id}`);
-
-    const { recipe } = data.data;
-    state.recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-
-    if (state.bookmarks.some(bookmark => bookmark.id === id))
-      state.recipe.bookmarked = true;
-    else state.recipe.bookmarked = false;
-    console.log(state.recipe);
-  } catch (err) {
-    console.error(`${err} 🎇🎇🎇`);
-    throw err;
-  }
-};*/
-
 export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
@@ -95,30 +67,6 @@ export const loadSearchResults = async function (query) {
     throw err;
   }
 };
-
-/*
-export const loadSearchResults = async function (query) {
-  try {
-    state.search.query = query;
-
-    const data = await getJSON(`${API_URL}?search=${query}`);
-    console.log(data);
-
-    state.search.results = data.data.recipes.map(rec => {
-      return {
-        id: rec.id,
-        title: rec.title,
-        publisher: rec.publisher,
-        sourceUrl: rec.source_url,
-        image: rec.image_url,
-      };
-    });
-    state.search.page = 1;
-  } catch (err) {
-    console.error(`${err} 🎇🎇🎇`);
-    throw err;
-  }
-};*/
 
 export const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
@@ -208,39 +156,3 @@ export const uploadRecipe = async function (newRecipe) {
     throw err;
   }
 };
-
-/*
-export const uploadRecipe = async function (newRecipe) {
-  try {
-    const ingredients = Object.entries(newRecipe)
-      .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
-      .map(ing => {
-        const ingArr = ing[1].replaceAll(' ', '').split(',');
-
-        if (ingArr.length !== 3)
-          throw new Error(
-            'Wrong ingredient format! Please use correct format :)'
-          );
-
-        const [quantity, unit, description] = ingArr;
-        return { quantity: quantity ? +quantity : null, unit, description };
-      });
-
-    const recipe = {
-      title: newRecipe.title,
-      source_url: newRecipe.sourceUrl,
-      image_url: newRecipe.image,
-      publisher: newRecipe.publisher,
-      cooking_time: +newRecipe.cookingTime,
-      servings: +newRecipe.servings,
-      ingredients,
-    };
-
-    const data = await sendJSON(`${API_URL}?key=${API_KEY}`, recipe);
-
-    console.log(data);
-  } catch (err) {
-    throw err;
-  }
-};
-*/
